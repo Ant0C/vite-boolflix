9e96dd4fbe8e46b8160d85bb0cee1cc41b391cad
Ant0C <cafeo97@gmail.com>
2023-03-09 11:56:33 +0100

    FIX star in  TV series

--- src/components/CardsSeries.vue
+++ src/components/CardsSeries.vue
@@ -15,18 +15,44 @@ export default {
         <img :src="`https://image.tmdb.org/t/p/w300/${series.poster_path}`" />
         <h3>{{ series.name }}</h3>
         <p>{{ series.original_name }}</p>
-        <span>
-            <p v-if="series.original_language === 'en'">
+        <ul>
+            <li v-if="series.original_language === 'en'">
                 <img src="/img/Bandiera-Inglese.png" alt="">
-            </p>
-            <p v-else-if="series.original_language === 'es'">
+            </li>
+            <li v-else-if="series.original_language === 'es'">
                 <img src="/img/bandiera-spagnola.jpg" alt="">
-            </p>
-            <p v-else>
+            </li>
+            <li v-else>
                 {{ series.original_language }}
-            </p>
-        </span>
-        <p>{{ series.vote_average }}</p>
+            </li>
+        </ul>
+        <ul>
+            <li v-show="parseInt(series.vote_average) === 1 || parseInt(series.vote_average) === 2">
+                <font-awesome-icon icon="fa-solid fa-star" />
+            </li>
+            <li v-show="parseInt(series.vote_average) === 3 || parseInt(series.vote_average) === 4">
+                <font-awesome-icon icon="fa-solid fa-star" />
+                <font-awesome-icon icon="fa-solid fa-star" />
+            </li>
+            <li v-show="parseInt(series.vote_average) === 5 || parseInt(series.vote_average) === 6">
+                <font-awesome-icon icon="fa-solid fa-star" />
+                <font-awesome-icon icon="fa-solid fa-star" />
+                <font-awesome-icon icon="fa-solid fa-star" />
+            </li>
+            <li v-show="parseInt(series.vote_average) === 7 || parseInt(series.vote_average) === 8">
+                <font-awesome-icon icon="fa-solid fa-star" />
+                <font-awesome-icon icon="fa-solid fa-star" />
+                <font-awesome-icon icon="fa-solid fa-star" />
+                <font-awesome-icon icon="fa-solid fa-star" />
+            </li>
+            <li v-show="parseInt(series.vote_average) === 9 || parseInt(series.vote_average) === 10">
+                <font-awesome-icon icon="fa-solid fa-star" />
+                <font-awesome-icon icon="fa-solid fa-star" />
+                <font-awesome-icon icon="fa-solid fa-star" />
+                <font-awesome-icon icon="fa-solid fa-star" />
+                <font-awesome-icon icon="fa-solid fa-star" />
+            </li>
+        </ul>
     </li>
 </template>
 
